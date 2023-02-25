71ad2462eb9ecdd82918ec88d055edc1624454f0
makoto14 <makoto.sasuke@gmail.com>
2023-02-26 03:49:42 +0900

    feat: add ThemeProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
+import { theme } from '../src/theme';
+import { ThemeProvider } from '@emotion/react';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
