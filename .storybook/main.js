5fe847834ed6e8287add77ab12459daf2e162196
makoto14 <makoto.sasuke@gmail.com>
2023-05-27 13:16:27 +0900

    chore: verup to storybook@7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,11 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
-    '@storybook/addon-interactions',
-    'storycap',
-  ],
-  framework: '@storybook/react',
-  babel: async (options) => ({
-    ...options,
-    presets: [...options.presets, '@emotion/babel-preset-css-prop'],
-  }),
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-interactions', 'storycap'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
