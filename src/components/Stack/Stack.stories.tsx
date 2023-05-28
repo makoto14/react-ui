5285fb5ef7cb44d23080fe6fc9079213f0d405d4
Makoto M <61872424+makoto14@users.noreply.github.com>
2023-05-28 13:55:54 +0900

    refactor: storybook file (#17)

--- src/components/Stack/Stack.stories.tsx
+++ src/components/Stack/Stack.stories.tsx
@@ -1,46 +1,42 @@
 import { css } from '@emotion/react';
-import { ComponentMeta, ComponentStory, StoryFn } from '@storybook/react';
-import { Fragment } from 'react';
+import { Meta, StoryObj } from '@storybook/react';
 import { Box } from '../Box';
 import { Stack } from './Stack';
 
 export default {
   title: 'components/Stack',
   component: Stack,
-} as ComponentMeta<typeof Stack>;
+} as Meta<typeof Stack>;
 
-const Template: ComponentStory<typeof Stack> = (args) => <Stack {...args} />;
-
-export const controls = Template.bind({});
-controls.args = {
-  spacing: 1,
-  children: [1, 2, 3, 4, 5, 6].map((elm) => (
-    <Box
-      key={elm}
-      css={css`
-        background-color: red;
-      `}
-    >
-      {elm}
-    </Box>
-  )),
+export const controls: StoryObj = {
+  args: {
+    spacing: 1,
+    children: [1, 2, 3, 4, 5, 6].map((elm) => (
+      <Box
+        key={elm}
+        css={css`
+          background-color: red;
+        `}
+      >
+        {elm}
+      </Box>
+    )),
+  },
 };
 
-export const Base: StoryFn = () => {
-  return (
-    <Fragment>
-      <Stack spacing={1} direction="row">
-        {[1, 2, 3, 4, 5, 6].map((elm) => (
-          <Box
-            key={elm}
-            css={css`
-              background-color: red;
-            `}
-          >
-            {elm}
-          </Box>
-        ))}
-      </Stack>
-    </Fragment>
-  );
+export const Base: StoryObj = {
+  args: {
+    spacing: 1,
+    direction: 'row',
+    children: [1, 2, 3, 4, 5, 6].map((elm) => (
+      <Box
+        key={elm}
+        css={css`
+          background-color: red;
+        `}
+      >
+        {elm}
+      </Box>
+    )),
+  },
 };
