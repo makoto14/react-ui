f362e43f0ed69c8138b000397c3677af5dc1fec0
Makoto M <61872424+makoto14@users.noreply.github.com>
2023-11-26 00:32:32 +0900

    fix: radix to ark (#22)
    * fix: radix to ark
    
    * fix: fix tsc error

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,12 +1,5 @@
+import { Dialog as ArkDialog, Portal } from '@ark-ui/react';
 import { css, useTheme } from '@emotion/react';
-import {
-  Content,
-  Description,
-  Overlay,
-  Portal,
-  Root,
-  Title,
-} from '@radix-ui/react-dialog';
 import { rgba } from 'polished';
 import { FC, ReactNode } from 'react';
 
@@ -26,10 +19,11 @@ export const Dialog: FC<DialogProps> = ({
   action,
 }) => {
   const theme = useTheme();
+
   return (
-    <Root open={open} onOpenChange={onOpenChange}>
+    <ArkDialog.Root open={open} onOpenChange={(e) => onOpenChange(e.open)}>
       <Portal>
-        <Overlay
+        <ArkDialog.Backdrop
           css={css`
             background-color: ${rgba(theme.colors.grey[400], 0.2)};
             backdrop-filter: blur(4px);
@@ -37,42 +31,45 @@ export const Dialog: FC<DialogProps> = ({
             inset: 0;
           `}
         />
-        <Content
-          css={css`
-            background-color: ${theme.colors.grey[0]};
-            border-radius: ${theme.radius.base}px;
-            box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
-              hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: 90vw;
-            max-width: 450px;
-            max-height: 85vh;
-            padding: 24px;
-          `}
-        >
-          <Title
+        <ArkDialog.Positioner>
+          <ArkDialog.Content
             css={css`
-              margin: 0;
-              font-weight: 500;
-              font-size: 18px;
+              background-color: ${theme.colors.grey[0]};
+              border-radius: ${theme.radius.base}px;
+              box-shadow:
+                hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
+                hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+              position: fixed;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              width: 90vw;
+              max-width: 450px;
+              max-height: 85vh;
+              padding: 24px;
             `}
           >
-            {title}
-          </Title>
-          <Description
-            css={css`
-              margin: 10px 0 20px;
-              font-size: 14px;
-            `}
-          >
-            {children}
-          </Description>
-          {action}
-        </Content>
+            <ArkDialog.Title
+              css={css`
+                margin: 0;
+                font-weight: 500;
+                font-size: 18px;
+              `}
+            >
+              {title}
+            </ArkDialog.Title>
+            <ArkDialog.Description
+              css={css`
+                margin: 10px 0 20px;
+                font-size: 14px;
+              `}
+            >
+              {children}
+            </ArkDialog.Description>
+            {action}
+          </ArkDialog.Content>
+        </ArkDialog.Positioner>
       </Portal>
-    </Root>
+    </ArkDialog.Root>
   );
 };
